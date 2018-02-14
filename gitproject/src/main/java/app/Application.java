737bad6b03fac79a44ae2ef9ce203fad882592ea
sbe <saddok.berna@gfi.fr>
2018-02-14 15:16:36 +0100

    app 2

--- gitproject/src/main/java/app/Application.java
+++ gitproject/src/main/java/app/Application.java
@@ -4,13 +4,13 @@ public class Application {
 	
 	
 
-//	public static void main(String[] args) {
-//
-//		addition(5,6);
-//
-//		System.out.println("app   ook");
-//
-//	}
+	public static void main(String[] args) {
+
+	//	addition(5,6);
+
+		System.out.println("app 2");
+
+	}
 
 	public int addition(int a, int b) {
 
